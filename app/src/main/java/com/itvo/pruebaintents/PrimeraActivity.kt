036683b3563f533154a41d0b743cff3ef03ff83a
dakedroid <kevin.molina.go@gmail.com>
2020-11-23 21:27:09 -0600

    Cambios

--- app/src/main/java/com/itvo/pruebaintents/PrimeraActivity.kt
+++ app/src/main/java/com/itvo/pruebaintents/PrimeraActivity.kt
@@ -1,7 +1,12 @@
 package com.itvo.pruebaintents
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class PrimeraActivity : AppCompatActivity() {
 
@@ -16,13 +21,34 @@ class PrimeraActivity : AppCompatActivity() {
 
     fun  abrirSegundaActivity(){
 
-        // TODO: CODIGO PARA REGRESAR A LA PRIMERA ACTIVITY
+        // EditText ya inicializado para que lo puedan utilizar.
+        val etNombre: EditText = findViewById(R.id.etNombre)
+        val btnEnviar: Button = findViewById(R.id.btnEnviar)
+        val txtEnviar: TextView = findViewById(R.id.txtEnviar)
+
+        // TODO: CODIGO PARA IR A LA SEGUNDA ACTIVITY
+
+
+        val nombre = etNombre.text.toString()
+
+        btnEnviar.setOnClickListener {
+            val intent = Intent(this, SegundaActivity::class.java)
+            intent.putExtra("nombre", nombre)
+            startActivity(intent)
+        }
+
+        txtEnviar.setOnClickListener {
+            val intent = Intent(this, SegundaActivity::class.java)
+            intent.putExtra("nombre", nombre)
+            startActivity(intent)
+        }
 
     }
 
 
     fun validarDatosIngresados(){
 
+        // CODIGO QUE NO HAY QUE PRECUPARSE
     }
 
 
--- app/src/main/java/com/itvo/pruebaintents/SegundaActivity.kt
+++ app/src/main/java/com/itvo/pruebaintents/SegundaActivity.kt
@@ -1,16 +1,38 @@
 package com.itvo.pruebaintents
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class SegundaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_segunda)
 
+        obtenerDatosDePrimeraActivity()
         abrirPrimeraActivity()
-        validarDatosIngresados()
+
+    }
+
+    fun obtenerDatosDePrimeraActivity(){
+
+        // TODO: CODIGO PARA OBTENER DATOS DE LA PRIMERA ACTIVITY
+
+
+
+        val txtNombreOtenido: TextView = findViewById(R.id.NombreObtenido)
+
+        var nombre  = intent.getStringExtra("nombre")
+
+        txtNombreOtenido.text = nombre
+
+
+
+
 
     }
 
@@ -18,16 +40,32 @@ class SegundaActivity : AppCompatActivity() {
     fun  abrirPrimeraActivity(){
 
 
-        // EditText ya inicializado para que lo puedan utilizar.
-        var etNombre: EditText = findViewById(R.id.etNombre)
+        // TODO: CODIGO PARA REGRESAR A LA PRIMERA ACTIVITY
 
-        // TODO: CODIGO PARA IR A LA SEGUNDA ACTIVITY
+
+        val btnRegresar: Button = findViewById(R.id.btnRegresar)
+        val txtRegresar: TextView = findViewById(R.id.txtRegresar)
+
+        btnRegresar.setOnClickListener {
+            val intent = Intent(this, PrimeraActivity::class.java)
+            startActivity(intent)
+        }
+
+        txtRegresar.setOnClickListener {
+            val intent = Intent(this, PrimeraActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+
 
     }
 
 
     fun validarDatosIngresados(){
 
+        // CODIGO QUE NO HAY QUE PREOCUPARSE
     }
 
 
